317f7608dddfca1fc519829e1bc06be295585da4
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-10-03 16:29:43 -0300

    teste

--- app/src/main/java/com/example/cartodepresente/MainActivity.kt
+++ app/src/main/java/com/example/cartodepresente/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,11 +18,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cartodepresente.ui.theme.CartãoDePresenteTheme
 import java.lang.reflect.GenericDeclaration
@@ -30,6 +35,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CartãoDePresenteTheme {
+            TelaCartaoPresente()
 
 
             }
@@ -40,41 +46,54 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TelaCartaoPresente(){
     Box {
-        Image(painter = painterResource(id = R.drawable.rosas),
-            contentDescription =null,
-            contentScale = ContentScale.Crop
+        Image(
+            painter = painterResource(id = R.drawable.rosas),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+            alpha = 0.8F
+
+
         )
 
 
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.End
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
             Text(
-                text = "Feliz Anivesário",
+                text = "Eu Adoro Você",
                 fontSize = 100.sp,
                 color = Color.Black,
                 fontFamily = FontFamily.Cursive,
                 fontWeight = FontWeight.Black,
-                textAlign = TextAlign.Center
-
-
+                textAlign = TextAlign.Center,
+                lineHeight = 100.sp,
+                modifier = Modifier
+                    .padding(top = 150.dp)
             )
-            Text(
-                text = "PARA: Ana Clara ",
-                fontSize = 30.sp,
-                color = Color.Black,
-                fontFamily = FontFamily.Cursive
 
 
-            )
+            Dedicatoria(Cauan = "De Cauan")
+            Dedicatoria(Cauan = "Para: Kamylle" )
         }
     }
+}
+@Composable
+fun Dedicatoria(Cauan: String){
+
+
+    Text(
+        text = Cauan,
+        fontSize = 30.sp,
+        color = Color.White,
+        fontWeight = FontWeight.Light,
+        fontFamily = FontFamily.Serif
+    )
 
 }
-
 
 
 
